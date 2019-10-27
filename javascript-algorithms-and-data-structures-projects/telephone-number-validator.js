f99e1412406dd8b17b4b74c9b979a755f47ba2e3
Lindsay Lopez <lopez95@berkeley.edu>
2019-10-27 16:29:31 -0700

    completed telephoneCheck

---
+++ javascript-algorithms-and-data-structures-projects/telephone-number-validator.js
@@ -0,0 +1,62 @@
+/*
+Telephone Number Validator
+
+Return true if the passed string looks like a valid US phone number.
+
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
+
+telephoneCheck("555-555-5555") should return a boolean.
+telephoneCheck("1 555-555-5555") should return true.
+telephoneCheck("1 (555) 555-5555") should return true.
+telephoneCheck("5555555555") should return true.
+telephoneCheck("555-555-5555") should return true.
+telephoneCheck("(555)555-5555") should return true.
+telephoneCheck("1(555)555-5555") should return true.
+telephoneCheck("555-5555") should return false.
+telephoneCheck("5555555") should return false.
+telephoneCheck("1 555)555-5555") should return false.
+telephoneCheck("1 555 555 5555") should return true.
+telephoneCheck("1 456 789 4444") should return true.
+telephoneCheck("123**&!!asdf#") should return false.
+telephoneCheck("55555555") should return false.
+telephoneCheck("(6054756961)") should return false
+telephoneCheck("2 (757) 622-7382") should return false.
+telephoneCheck("0 (757) 622-7382") should return false.
+telephoneCheck("-1 (757) 622-7382") should return false
+telephoneCheck("2 757 622-7382") should return false.
+telephoneCheck("10 (757) 622-7382") should return false.
+telephoneCheck("27576227382") should return false.
+telephoneCheck("(275)76227382") should return false.
+telephoneCheck("2(757)6227382") should return false.
+telephoneCheck("2(757)622-7382") should return false.
+telephoneCheck("555)-555-5555") should return false.
+telephoneCheck("(555-555-5555") should return false.
+telephoneCheck("(555)5(55?)-5555") should return false.
+
+My solution explanation:
+- Need ^ and $ to match the entire string from beginning to end
+- Can start with 1 and no space, one and space, or nothing
+- First group of three digits can have paranthesis or no paranthesis
+- Following the first group, can be a space, hyphen, or nothing
+- Second group of three digits can also be followed by space, hypen or nothing
+- Was there an easier way to do this? Maybe... Let's check the hints!
+
+Keep in mind next time:
+- Can use \d instead of [0-9] lol forgot
+- Can use "?" - makes the previous character optional
+*/
+function telephoneCheck(str) {
+  let telephoneRegex = /^(1|1 |)(\([0-9]{3}\)|[0-9]{3})( |\-|)[0-9]{3}( |\-|)[0-9]{4}$/;
+  return telephoneRegex.test(str);
+}
+
+console.log(telephoneCheck("(555)5(55?)-5555"));
